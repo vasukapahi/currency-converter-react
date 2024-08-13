e580c9f81fca6a8a36e5406b1d9d58f06f15182c
vasu kapahi <vasukapahi16@gmail.com>
2024-08-13 15:06:36 +0530

    tailwind setup and input boxes

---
+++ currency-converter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,11 @@
+import {useEffect,useState} from "react";
+ export default function useCurrencyInfo(currency){
+    const [data, setData] = useState({});
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    },[currency])  
+    return data; 
+ }
